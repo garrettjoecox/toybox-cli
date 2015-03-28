f8e460e9c6c151b094d360785bac5fdd9437bf89
garrettjoecox <garrettjcox@gmail.com>
2015-03-28 09:41:25 -0700

    Refactor 0.2.0

--- bin/toybox.js
+++ bin/toybox.js
@@ -1,39 +1,18 @@
 #!/usr/bin/env node
 
-/*
- * toybox-cli
- * https://github.com/garrettjoecox/toybox-cli
- *
- * Copyright (c) 2015, Garrett Cox
- * Licensed under the MIT license.
- */
-
-/**
- * Module dependencies.
- */
-
-var program = require('commander'),
-    updateNotifier = require('update-notifier'),
-    Insight = require('insight'),
-    _ = require('underscore'),
-    banner = require('../lib/banner.js'),
-    Api = require('..'),
-    api = new Api('access_token'),
-    path = require('path'),
-    debug = require('../lib/debugger.js'),
-    pkg = require('../package.json'),
-    gulp = require('gulp'),
-    fs = require('fs'),
-    gulpGulp = require('gulp-gulp'),
-    ginstall = require('gulp-install'),
-    sequence = require('gulp-sequence'),
-    shell = require('gulp-shell'),
-    inquirer = require('inquirer')
-
+var sequence = require('gulp-sequence');
+var ginstall = require('gulp-install');
+var gulpGulp = require('gulp-gulp');
+var program = require('commander');
+var inquirer = require('inquirer');
+var shell = require('gulp-shell');
+var path = require('path');
+var gulp = require('gulp');
+var fs = require('fs');
 require('colors');
 
-if(process.argv.length === 2){
-    console.log("Thanks for using Toybox! (By Garrett Cox)".grey);
+if(process.argv.length < 3){
+    console.log("Thanks for using toybox! (By Garrett Cox)".grey);
     console.log("Run 'toybox init' to get started!".grey);
 }
 
@@ -41,52 +20,75 @@ program
     .command('init')
     .action(function(){
         inquirer.prompt([{type:'input',name:'username',message:'Enter Github username!'},{type:'input',name:'date',message:'When did you start @ HR? (2015-02, 2014-12, etc)'}], function(answers){
-            gulp.task('copy', copy())
-            gulp.task('clone', clone(answers.username, answers.date)).start();
-        })
-    })
+            gulp.task('copy', copy());
+            gulp.task('git', git(answers.username, answers.date)).start();
+        });
+    });
 
 program
     .command('start')
     .action(function(){
         if(fs.existsSync(process.cwd()+'/gulpfile.js')){
-            gulp.task('open', open())
+            gulp.task('open', open());
             gulp.task('runGulpFile', runGulp()).start();
         }else{
-            console.log("You're not in a Toybox!".bold.red);
-            console.log("run 'Toybox new'".grey);
+            console.log("You're not in a toybox!".bold.red);
+            console.log("run 'toybox init'".grey);
         }
     });
 
+program
+    .command('pull')
+    .action(function(){
+        gulp.task('pull', pull()).start();
+    });
 
-function runGulp(){
+program
+    .command('push')
+    .action(function(){
+        inquirer.prompt([{type:'input', name:'commit', message:'Enter commit message for GitHub!'}], function(answers){
+            gulp.task('push', push(answers.commit)).start();
+        });
+    });
+
+function copy(){
     return function(){
-        return gulp.src(process.cwd()+'/gulpfile.js')
-            .pipe(gulpGulp());
-    }
+        var toybox = path.join(__dirname, '../lib/toybox/**/*');
+        return gulp.src(toybox)
+            .pipe(gulp.dest(process.cwd()+'/toybox'))
+            .pipe(ginstall());
+    };
 }
 
-function clone(username, date){
+function git(username, date){
     return shell.task([
-        'git clone https://github.com/' + username + '/' + date + '-toy-problems.git toybox/toy-problems-repo'
+        'git clone https://github.com/' + username + '/' + date + '-toy-problems.git toybox/toy-problems-repo; cd toybox/toy-problems-repo/ && git remote add upstream https://github.com/hackreactor/' + date + '-toy-problems.git'
     ]);
 }
 
 function open(){
     return shell.task([
-        'subl .'
+        'subl toy-problems-repo'
     ]);
 }
 
-function copy(){
+function runGulp(){
     return function(){
-        var toybox = path.join(__dirname, '../lib/ToyBox/**/*');
-        return gulp.src(toybox)
-            .pipe(gulp.dest(process.cwd()+'/toybox'))
-            .pipe(ginstall());
-    }
+        return gulp.src(process.cwd()+'/gulpfile.js')
+            .pipe(gulpGulp());
+    };
 }
 
+function pull(){
+    return shell.task([
+        'cd toy-problems-repo && git pull upstream master'
+    ]);
+}
 
+function push(commit){
+    return shell.task([
+        'cd toy-problems-repo && git add .; git commit -m "' + commit + '"; git push origin master'
+    ]);
+}
 
 program.parse(process.argv);
